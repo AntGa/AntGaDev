edd4a2515a7b343be103c8962e159d4f1790ecc5
Anton Garay <antonvince.garay@gmail.com>
2024-08-07 15:33:40 +1200

    Added parralax again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,58 @@
 'use client'
 
-import { motion, useSpring, useTransform } from 'framer-motion'
+import { motion, useSpring } from 'framer-motion'
 import { useGesture } from '@use-gesture/react'
 import { useState, useEffect } from 'react'
 import Background from './components/meshblobs/background'
 import { Navbar } from './components/Navbar/Navbar'
 
+interface TargetPosition {
+  x: number
+}
+
 export default function Home() {
+  const [target, setTarget] = useState<TargetPosition>({ x: 0 })
+  const x = useSpring(0)
+
+  useEffect(() => {
+    x.set(target.x)
+  }, [target, x])
+
+  const bind = useGesture({
+    onMove: ({ xy: [px], dragging }) => {
+      if (!dragging) {
+        const rect = document.body.getBoundingClientRect()
+        const newX = (px - rect.width / 2) / (rect.width / 2)
+        setTarget({ x: newX * 30 })
+      }
+    },
+  })
+
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-[#F5E6D7]">
+    <div
+      className="flex h-screen w-screen flex-col items-center justify-center bg-[#F5E6D7]"
+      {...bind()}
+    >
       <motion.div
-        initial={{ width: 0, height: 0, rotate: -15, borderRadius: 50 }}
+        initial={{
+          width: '0',
+          height: '0',
+          rotate: -15,
+          borderRadius: 50,
+        }}
         animate={{
-          width: '100vw',
+          width: '105%',
           height: '100vh',
           rotate: 0,
           borderRadius: 0,
         }}
         transition={{ ease: [0.83, 0, 0.17, 1], duration: 1.5, delay: 0.5 }}
-        className="background flex justify-center overflow-hidden"
+        className="background absolute flex justify-center"
+        style={{ x }}
       >
-        <Navbar></Navbar>
+        <Navbar />
       </motion.div>
+      <div className="h-screen w-screen bg-black"></div>
     </div>
   )
 }
