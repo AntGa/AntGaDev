c151453c0c3eaba41a697680f6aa34d339474496
Anton Garay <antonvince.garay@gmail.com>
2024-08-08 22:48:06 +1200

    Started to redesign  Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,29 @@
 'use client'
 
-import { motion, useSpring } from 'framer-motion'
-import { useGesture } from '@use-gesture/react'
-import { useState, useEffect } from 'react'
-import Background from './components/meshblobs/background'
-import { Navbar } from './components/Navbar/Navbar'
-
-interface TargetPosition {
-  x: number
-}
+import { useEffect } from 'react'
 
 export default function Home() {
-  const [target, setTarget] = useState<TargetPosition>({ x: 0 })
-  const x = useSpring(0)
-
   useEffect(() => {
-    x.set(target.x)
-  }, [target, x])
-
-  const bind = useGesture({
-    onMove: ({ xy: [px], dragging }) => {
-      if (!dragging) {
-        const rect = document.body.getBoundingClientRect()
-        const newX = (px - rect.width / 2) / (rect.width / 2)
-        setTarget({ x: newX * 30 })
-      }
-    },
-  })
+    ;(async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default
+      const locomotiveScroll = new LocomotiveScroll()
+    })()
+  }, [])
 
   return (
-    <div
-      className="flex h-screen w-screen flex-col items-center justify-center bg-[#F5E6D7]"
-      {...bind()}
-    >
-      <motion.div
-        initial={{
-          width: '0',
-          height: '0',
-          rotate: -15,
-          borderRadius: 50,
-        }}
-        animate={{
-          width: '105%',
-          height: '100vh',
-          rotate: 0,
-          borderRadius: 0,
-        }}
-        transition={{ ease: [0.83, 0, 0.17, 1], duration: 1.5, delay: 0.5 }}
-        className="background absolute flex justify-center"
-        style={{ x }}
-      >
-        <Navbar />
-      </motion.div>
-      <div className="h-screen w-screen bg-black"></div>
+    <div className="flex flex-col gap-6">
+      <div className="flex h-screen items-center bg-[#2D2D2D]">
+        <p
+          data-scroll
+          data-scroll-speed="0.2"
+          className="font-semi ml-[140px] text-9xl text-[#FFFCED]"
+        >
+          ANTON GARAY
+        </p>
+      </div>
+      <div className="h-screen bg-[#2D2D2D]"></div>
+      <div className="h-screen bg-[#2D2D2D]"></div>
+      <div className="h-screen bg-[#2D2D2D]"></div>
     </div>
   )
 }
