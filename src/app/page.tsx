c1b481025e0abc51f6fb2987a0f6e95b9c653fa0
AntGa <antonvince.garay@gmail.com>
2024-08-15 23:28:12 +1200

    Added test animation (not happy)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client"
 
-import { useEffect } from 'react'
-export const runtime = 'edge'
+import { useEffect } from "react"
+import { motion } from "framer-motion"
+
+export const runtime = "edge"
 export default function Home() {
   useEffect(() => {
     ;(async () => {
-      const LocomotiveScroll = (await import('locomotive-scroll')).default
+      const LocomotiveScroll = (await import("locomotive-scroll")).default
       const locomotiveScroll = new LocomotiveScroll()
     })()
   }, [])
@@ -13,20 +15,58 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-6 overflow-hidden">
       <div className="flex h-screen flex-col items-center justify-center bg-[#202123]">
-        <p
+        <div
           data-scroll
           data-scroll-speed="0.15"
-          className="font-semi ml-[140px] w-full text-9xl text-white"
+          className="clipped relative flex w-full items-center justify-center text-9xl text-white"
         >
-          ANTON GARAY
-        </p>
-        <p
-          className="ml-[850px] w-full text-3xl text-white"
-          data-scroll
-          data-scroll-speed="0.14"
-        >
-          Game Maker & Full Stack Developer
-        </p>
+          {/* First Stage: ANT GA */}
+          <motion.span
+            initial={{ y: 140 }}
+            animate={{ y: 0, x: -224 }}
+            transition={{
+              ease: [0.22, 1, 0.36, 1],
+              duration: 1.2,
+              x: { delay: 1.2, duration: 1.2, ease: [0.22, 1, 0.36, 1] },
+            }}
+            className="tracking-tight"
+          >
+            ANT
+          </motion.span>
+          <motion.span
+            initial={{ y: 140 }}
+            animate={{ y: 0 }}
+            transition={{
+              ease: [0.22, 1, 0.36, 1],
+              duration: 1.2,
+            }}
+            className="tracking-tight"
+          >
+            GA
+          </motion.span>
+
+          {/* Second Stage: Adding ON to ANT and RAY to GA */}
+          <motion.span
+            initial={{ y: 140, opacity: 0, x: -100 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              y: { delay: 1.4, duration: 1.2, ease: [0.22, 1, 0.36, 1] },
+            }}
+            className="absolute tracking-tight"
+          >
+            ON
+          </motion.span>
+          <motion.span
+            initial={{ y: 140, opacity: 0, x: 330 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              y: { delay: 1.4, duration: 1.2, ease: [0.22, 1, 0.36, 1] },
+            }}
+            className="absolute tracking-tight"
+          >
+            RAY
+          </motion.span>
+        </div>
       </div>
       <div className="h-screen bg-[#2D2D2D]"></div>
       <div className="h-screen bg-[#2D2D2D]"></div>
---
+++ src/app/utils/Split.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Split = () => {
+  return <div>Split</div>
+}
